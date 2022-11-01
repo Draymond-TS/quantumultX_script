c16f36275ca3b00ab84ce5b30a507aa5793a68dc
Draymond <38874399+Draymond-TS@users.noreply.github.com>
2022-11-01 17:46:18 +0800

    Delete bilibili.js

--- rewrite/TVB/bilibili.js
+++
@@ -1,5 +0,0 @@
-# 哔哩哔哩番剧开启1080P+ （by NobyDa）
-^https:\/\/ap(p|i)\.bilibili\.com\/((pgc\/player\/api\/playurl)|(x\/v2\/account\/myinfo\?)|(x\/v2\/account/mine\?)) url script-response-body https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/master/js/backup/bilifj.js
-
-
-hostname = ap?.bilibili.com
