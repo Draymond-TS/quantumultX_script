1695631c12b237091012633b30bb32b412f857f0
Draymond <38874399+Draymond-TS@users.noreply.github.com>
2022-12-05 17:38:28 +0800

    Create yuncai.js

---
+++ rewrite/yuncai/yuncai.js
@@ -0,0 +1,5 @@
+
+hostname = biz.caiyunapp.com
+
+#彩云（解锁VIP）   biz.caiyunapp.com
+^https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) url script-response-body https://raw.githubusercontent.com/deezertidal/private/master/js-backup/Crack/caiyun.js
